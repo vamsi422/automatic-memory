301aebc6124f1e54733be84510e7c7d9d152b551
vamshi <vamshikrishna7942@gmail.com>
2024-03-17 14:42:20 +0530

    git data added

--- index.js
+++ index.js
@@ -4,6 +4,40 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
+const git_data = {
+  login: "vamsi422",
+  id: 68322728,
+  node_id: "MDQ6VXNlcjY4MzIyNzI4",
+  avatar_url: "https://avatars.githubusercontent.com/u/68322728?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/vamsi422",
+  html_url: "https://github.com/vamsi422",
+  followers_url: "https://api.github.com/users/vamsi422/followers",
+  following_url: "https://api.github.com/users/vamsi422/following{/other_user}",
+  gists_url: "https://api.github.com/users/vamsi422/gists{/gist_id}",
+  starred_url: "https://api.github.com/users/vamsi422/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/vamsi422/subscriptions",
+  organizations_url: "https://api.github.com/users/vamsi422/orgs",
+  repos_url: "https://api.github.com/users/vamsi422/repos",
+  events_url: "https://api.github.com/users/vamsi422/events{/privacy}",
+  received_events_url: "https://api.github.com/users/vamsi422/received_events",
+  type: "User",
+  site_admin: false,
+  name: "vamshi",
+  company: null,
+  blog: "",
+  location: "India, Hyderabad",
+  email: null,
+  hireable: null,
+  bio: "I am a web developer working on MERN stack projects",
+  twitter_username: null,
+  public_repos: 18,
+  public_gists: 0,
+  followers: 0,
+  following: 1,
+  created_at: "2020-07-15T08:00:14Z",
+  updated_at: "2024-03-17T08:46:53Z",
+};
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -17,6 +51,9 @@ app.get("/login", (req, res) => {
 });
 app.get("/youtube", (req, res) => {
   res.send("<h1>YOU_tube</h1>");
+});
+app.get("/git_hub", (req, res) => {
+  res.json(git_data);
 });
 app.listen(process.env.PORT, (req, res) => {
   console.log(`Example app listening on port ${process.env.PORT}`);
